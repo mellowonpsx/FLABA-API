491fd690d5b6259062ec0947e4a08eac58e5d109
Sergio L. <mellowonpsx@gmail.com>
2015-07-10 23:06:13 +0200

    changed sha512 to base64 for compatibility

--- db/users.inc.php
+++ db/users.inc.php
@@ -1,1 +1,1 @@
-{"username":"admin","password":"password","auth":"Basic YB+PIrFTIaPNNCwdUMbOjaFT2pcNPb/iWz2/qTJsOzCsMthyXYUT/EOl56l/Y3xQKgofBcmX6/TedylnbbpW0g==","level":"admin","id":"1","_id":"gmghVVB9QFGT0S3o"}
+{"username":"admin","password":"password","auth":"Basic YWRtaW46cGFzc3dvcmQ=","level":"admin","id":"1","_id":"gmghVVB9QFGT0S3o"}
